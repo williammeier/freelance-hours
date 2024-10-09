94da91c3277a281cd9671c6539cfa2d6f85571b4
William Meier <william.meier@outlook.com>
2024-10-09 19:10:16 -0300

    avisando o componentes proposal que uma nova proposta foi criada

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -4,12 +4,36 @@ namespace App\Livewire\Projects;
 
 use App\Models\Project;
 use App\Models\Proposal;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Proposals extends Component
 {
     public Project $project;
 
+    public int $qty = 10;
+
+    #[Computed()]
+    public function proposals()
+    {
+        return $this->project->proposals()
+            ->orderBy('hours')
+            ->paginate($this->qty);
+    }
+
+    #[Computed()]
+    public function lastProposalTime()
+    {
+        return $this->project->proposals()->latest()->first()->created_at->diffForHumans();
+    }
+
+    public function loadMore()
+    {
+        $this->qty += 10;
+    }
+
+    #[On('proposal::created')]
     public function render()
     {
         return view('livewire.projects.proposals');
--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -32,6 +32,7 @@ class Create extends Component
             ['hours' => $this->hours]
         );
 
+        $this->dispatch('proposal::created');
         $this->modal = false;
     }
 
